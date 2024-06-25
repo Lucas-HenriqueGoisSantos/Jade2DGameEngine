588370416926b6c559393ec73f50db73bbe3bc1b
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-06-24 22:25:37 -0300

    Inicializing SDL and making windows

--- Jade2DGameEngine/src/Game.cpp
+++ Jade2DGameEngine/src/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include <iostream>
+#include <SDL.h>
 
 Game::Game() {
 	std::cout << "It works!!" << std::endl;
@@ -12,6 +13,22 @@ Game::~Game() {
 
 void Game::Inicialize() {
 
+	if ( SDL_Init(SDL_INIT_EVERYTHING) != 0 ) {
+		std::cerr << "Error inicializing SDL." << std::endl;
+		return;
+	}
+
+	SDL_Window* window = SDL_CreateWindow( NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS );
+	if ( !window ) {
+		std::cerr << "Error creating SDL window." << std::endl;
+		return;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, 0 );
+	if (!renderer) {
+		std::cerr << "Error creating SDL renderer." << std::endl;
+		return;
+	}
 }
 
 void Game::Run() {
