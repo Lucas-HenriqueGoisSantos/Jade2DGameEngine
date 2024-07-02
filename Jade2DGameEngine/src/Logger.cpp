98b9f4fe805083c2a44bc2f8baaadc9f64a615bf
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-02 09:37:03 -0300

    Improved the Logging and error messages

--- Jade2DGameEngine/src/Logger.cpp
+++ Jade2DGameEngine/src/Logger.cpp
@@ -1,26 +1,27 @@
 #include "Logger.h"
 #include <iostream>
+#include <string>
+#include <chrono>
 #include <ctime>
 
-
+std::string CurrentDateTimeToString() {
+    std::time_t now = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now() );
+    std::string output( 30, '\0' );
+    std::strftime( &output[0], output.size(), "%d-%b-y %H:%M:%S", std::localtime( &now ) );
+    return output;
+}
 void Logger::Log( const std::string& message ) {
-    // Get the current time in seconds since January 1, 1970
-    time_t now = time(0);
 
-    // Convert the time to a string
-    char* date_time = ctime(&now);
+    std::string output = "LOG: [" + CurrentDateTimeToString() + "]: " + message;
 
     // Print the current date and time
-    std::cout << "\033[1;32m" << date_time << message << "\033[0m" << std::endl;
+    std::cout << "\033[1;32m" << output << "\033[0m" << std::endl;
 }
 
 void Logger::Err( const std::string& message ) {
-    // Get the current time in seconds since January 1, 1970
-    time_t now = time(0);
 
-    // Convert the time to a string
-    char* date_time = ctime(&now);
+    std::string output = "ERR: [" + CurrentDateTimeToString() + "]: " + message;
 
     // Print the current date and time
-    std::cout << "\033[1;31m" << date_time << message << "\033[0m" << std::endl;
+    std::cout << "\033[1;31m" << output << "\033[0m" << std::endl;
 }
