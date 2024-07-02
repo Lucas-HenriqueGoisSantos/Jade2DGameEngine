f9bed04464a97be91051b9ddfa9429ef910bc0c3
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-02 09:57:37 -0300

    correcting a small error in the logger

--- Jade2DGameEngine/src/Logger.cpp
+++ Jade2DGameEngine/src/Logger.cpp
@@ -4,6 +4,9 @@
 #include <chrono>
 #include <ctime>
 
+std::vector<LogEntry> Logger::messages;
+
+
 std::string CurrentDateTimeToString() {
     std::time_t now = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now() );
     std::string output( 30, '\0' );
