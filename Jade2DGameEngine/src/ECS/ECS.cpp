acda9f5350a888fe41441565b4917e8cfe45b237
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-07 16:47:31 -0300

    Implemented GetSystemEntities

--- Jade2DGameEngine/src/ECS/ECS.cpp
+++ Jade2DGameEngine/src/ECS/ECS.cpp
@@ -1,5 +1,25 @@
 #include "ECS.h"
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Entity
 int Entity::GetId() const {
 	return id;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// System
+void System::AddEntityToSystem( Entity entity ) {
+
+}
+
+void System::RemoveEntityFromSystem( Entity entity ) {
+
+}
+
+std::vector<Entity> System::GetSystemEntities() const {
+	return entities;
+}
+
+Signature& System::GetComponentSignature() const {
+
 }
