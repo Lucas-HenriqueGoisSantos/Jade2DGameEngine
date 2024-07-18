e2fe15e256361091633edf3556154c60a096756a
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-07-18 14:56:54 -0300

    Implementing the movement system

--- Jade2DGameEngine/src/Systems/MovementSystem.h
+++ Jade2DGameEngine/src/Systems/MovementSystem.h
@@ -1,16 +1,29 @@
 #ifndef MOVEMENTSYSTEM_H
 #define MOVEMENTSYSTEM_H
 
+#include "../ECS/ECS.h"
+#include "../Components/TransformComponent.h"
+#include "../Components/RigidBodyComponent.h"
+
 class MovementSystem : public System {
+
 public:
 	MovementSystem() {
-		// RequiredComponent<TransformComponent>();
-		// RequiredComponent<VelocityComponent>();
-	}
-	void Update() {
-	/*	for ( auto entity : GetEntities() ) {
 
-		}*/
+		RequiredComponent<TransformComponent>();
+		RequiredComponent<RigidBodyComponent>();
+	}
+
+	void Update() {
+
+		for ( auto entity : GetSystemEntities() ) {
+
+			auto& transform = entity.GetComponent<TransformComponent>();
+			auto rigidbody = entity.GetComponent<RigidBodyComponent>();
+
+			transform.position.x += rigidbody.velocity.x;
+			transform.position.y += rigidbody.velocity.y;
+		}
 	}
 };
 
