3cbcb6686d38332b20c6ac14306a66ad9fe67d5e
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-07-19 20:11:43 -0300

    Small fix

--- Jade2DGameEngine/src/Components/SpriteComponent.h
+++ Jade2DGameEngine/src/Components/SpriteComponent.h
@@ -7,7 +7,7 @@ struct SpriteComponent {
 	int width;
 	int height;
 
-	SpriteComponent( int width, int height ) {
+	SpriteComponent( int width = 0, int height = 0 ) {
 		
 		this->width = width;
 		this->height = height;
