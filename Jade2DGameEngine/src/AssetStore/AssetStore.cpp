2a441d3b129537472d18c74bd4ea6eba7f760b26
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-07-20 18:30:35 -0300

    Implementing AddTexture

--- Jade2DGameEngine/src/AssetStore/AssetStore.cpp
+++ Jade2DGameEngine/src/AssetStore/AssetStore.cpp
@@ -1,5 +1,6 @@
 #include "AssetStore.h"
 #include "../Logger/Logger.h"
+#include <SDL_image.h>
 
 
 AssetStore::AssetStore() {
@@ -19,6 +20,8 @@ void AssetStore::ClearAssets() {
 
 void AssetStore::AddTexture( const std::string& assetId, const std::string& filePath ) {
 
+	SDL_Surface* surface = IMG_Load( filePath.c_str() );
+	SDL_Texture* texture = SDL_CreateTextureFromSurface( surface );
 }
 
 SDL_Texture* AssetStore::GetTexture( const std::string& assetId ) const {
