f341acef6d49ee60aabdbbb367efe5afbb34e11d
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-19 23:11:01 -0300

    Prototyping AddTexture

--- Jade2DGameEngine/src/Game/Game.cpp
+++ Jade2DGameEngine/src/Game/Game.cpp
@@ -29,13 +29,28 @@ Game::~Game() {
 
 void Game::Setup() {
 
+	// Add the systems here
 	registry->AddSystem<MovementSystem>();
+	
 	registry->AddSystem<RenderSystem>();
 
+
+	// Add assets here
+	assetStore->AddTexture( "tank-image", "./assets/images/tank-panther-right.png" );
+	
+	assetStore->AddTexture( "truck-image", "./assets/images/truck-ford-right.png" );
+
+
+	// Create and add components to entities here
 	Entity tank = registry->CreateEntity();
 	tank.AddComponent<TransformComponent>( glm::vec2( 10.0, 30.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
 	tank.AddComponent<RigidBodyComponent>( glm::vec2( 50.0, 0.0 ) );
-	tank.AddComponent<SpriteComponent>( 10, 10 );
+	tank.AddComponent<SpriteComponent>( "tank-image", 10, 10 );
+
+	Entity truck = registry->CreateEntity();
+	tank.AddComponent<TransformComponent>( glm::vec2( 50.0, 100.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
+	tank.AddComponent<RigidBodyComponent>( glm::vec2( 50.0, 0.0 ) );
+	tank.AddComponent<SpriteComponent>( "truck-image", 10, 10 );
 }
 
 
