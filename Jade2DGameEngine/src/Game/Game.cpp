5c0bb4785d00df1fcfd2af9f187e1a858a080e79
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-21 22:14:11 -0300

    Reading Tilemap from file

--- Jade2DGameEngine/src/Game/Game.cpp
+++ Jade2DGameEngine/src/Game/Game.cpp
@@ -8,6 +8,8 @@
 #include <glm/glm.hpp>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <iostream>
+#include <fstream>
 
 
 
@@ -23,6 +25,7 @@ Game::Game() {
 
 Game::~Game() {
 
+	Destroy();
 	Logger::Log( "Game destructor called correctly!" );
 }
 
@@ -84,9 +87,31 @@ void Game::LoadLevel( int level ) {
 
 	// Add assets here
 	assetStore->AddTexture( renderer, "tank-image", "./assets/images/tank-panther-right.png" );
-
 	assetStore->AddTexture( renderer, "truck-image", "./assets/images/truck-ford-right.png" );
+	assetStore->AddTexture( renderer, "tilemap-image", "./assets/tilemaps/jungle.png" );
 
+	// Load Tilemap
+	int tileSize = 32;
+	double tileScale = 1.0;
+	int mapNumCols = 25;
+	int mapNumRows = 20;
+	std::fstream mapFile;
+	mapFile.open( "./assets/tilemaps/jungle.map" );
+	for (int y = 0; y < mapNumRows; y++) {
+		for (int x = 0; x < mapNumCols; x++) {
+			char ch;
+			mapFile.get(ch);
+			int srcRectY = std::atoi(&ch) * tileSize;
+			mapFile.get(ch);
+			int srcRectX = std::atoi(&ch) * tileSize;
+			mapFile.ignore();
+
+			Entity tile = registry->CreateEntity();
+			tile.AddComponent<TransformComponent>(glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)), glm::vec2(tileScale, tileScale), 0.0);
+			tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, srcRectX, srcRectY);
+		}
+	}
+	mapFile.close();
 
 	// Create and add components to entities here
 	Entity tank = registry->CreateEntity();
