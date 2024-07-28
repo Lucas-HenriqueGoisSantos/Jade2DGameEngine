7cdb376c968c2f727cbb74bcdbcc6967909c311b
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-28 17:26:28 -0300

    Inicialized Collision Event variables

--- Jade2DGameEngine/src/Events/CollisionEvent.h
+++ Jade2DGameEngine/src/Events/CollisionEvent.h
@@ -1,8 +1,17 @@
 #ifndef COLLISIONEVENT_H
 #define COLLISIONEVENT_H
 
+
+#include "../ECS/ECS.h"
+
+
 class CollisionEvent {
 
+public:
+	Entity a;
+	Entity b;
+
+	CollisionEvent( Entity a, Entity b ) : a( a ), b( b ) {}
 };
 
 
