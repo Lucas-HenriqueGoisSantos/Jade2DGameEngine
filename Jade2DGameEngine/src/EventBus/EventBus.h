e4a668bfaccd762bee78768c0f0013dbbc407555
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-28 17:34:04 -0300

    Logging the event bus

--- Jade2DGameEngine/src/EventBus/EventBus.h
+++ Jade2DGameEngine/src/EventBus/EventBus.h
@@ -2,11 +2,20 @@
 #define EVENTBUS_H
 
 
+#include "../Logger/Logger.h"
+
+
 class EventBus {
 
 public:
 	EventBus() {
 
+		Logger::Log( "EventBus constructor called!" );
+	}
+
+	~EventBus() {
+
+		Logger::Log( "EventBus destructor called!" );
 	}
 };
 
