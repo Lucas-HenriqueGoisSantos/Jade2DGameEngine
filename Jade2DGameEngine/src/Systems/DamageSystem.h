9c690cd06e5c6a4bf9d6cbcf50e45adb15a1e1c3
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-29 23:25:00 -0300

    Created a Damage System file

---
+++ Jade2DGameEngine/src/Systems/DamageSystem.h
@@ -0,0 +1,23 @@
+#ifndef DAMAGESYSTEM_H
+#define DAMAGESYSTEM_H
+
+
+#include "../ECS/ECS.h"
+#include "../Components/BoxColliderComponent.h"
+
+
+class DamageSystem : public System {
+
+public:
+	DamageSystem() {
+
+		RequireComponent<BoxColliderComponent>();
+	}
+
+	void Update() {
+
+	}
+};
+
+
+#endif // !DAMAGESYSTEM_H
