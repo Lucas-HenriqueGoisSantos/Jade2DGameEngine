cd2612edcda29ac65716b7c341c56f5d90939758
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-07-31 21:57:50 -0300

    Inicializing keyboard controlled component variables

--- Jade2DGameEngine/src/Components/KeyboardControlledComponent.h
+++ Jade2DGameEngine/src/Components/KeyboardControlledComponent.h
@@ -12,8 +12,12 @@ struct KeyboardContrelledComponent {
     glm::vec2 downVelocity;
     glm::vec2 leftVelocity;
 
-    KeyboardContrelledComponent() {
+    KeyboardContrelledComponent( glm::vec2 upVelocity = glm::vec2( 0 ), glm::vec2 rightVelocity = glm::vec2( 0 ), glm::vec2 downVelocity = glm::vec2( 0 ), glm::vec2 leftVelocity = glm::vec2( 0 ) ) {
 
+        this->upVelocity = upVelocity;
+        this->rightVelocity = rightVelocity;
+        this->downVelocity = downVelocity;
+        this->leftVelocity = leftVelocity;
     }
 };
 
