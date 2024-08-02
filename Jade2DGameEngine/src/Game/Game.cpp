afa82bc7bdff71d1eafdd009dcefed7e209cbddb
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-02 10:40:01 -0300

    Adding an isFixed property to sprites

--- Jade2DGameEngine/src/Game/Game.cpp
+++ Jade2DGameEngine/src/Game/Game.cpp
@@ -141,7 +141,7 @@ void Game::LoadLevel( int level ) {
 
 			Entity tile = registry->CreateEntity();
 			tile.AddComponent<TransformComponent>(glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)), glm::vec2(tileScale, tileScale), 0.0);
-			tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, 0, srcRectX, srcRectY);
+			tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, 0, false, srcRectX, srcRectY);
 		}
 	}
 	mapFile.close();
@@ -161,7 +161,7 @@ void Game::LoadLevel( int level ) {
 	Entity radar = registry->CreateEntity();
 	radar.AddComponent<TransformComponent>( glm::vec2( 500.0, 10.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
 	radar.AddComponent<RigidBodyComponent>( glm::vec2( 0.0, 0.0 ) );
-	radar.AddComponent<SpriteComponent>( "radar-image", 64, 64, 3 );
+	radar.AddComponent<SpriteComponent>( "radar-image", 64, 64, 3, true );
 	radar.AddComponent<AnimationComponent>( 7, 8 );
 
 	Entity tank = registry->CreateEntity();
