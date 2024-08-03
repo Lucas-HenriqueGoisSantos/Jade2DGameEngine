ac2cc2d9767fa71a3207fab002bd0846d78ad49b
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-08-03 20:53:14 -0300

    Implementing the ProjectileEmitterComponent variables

--- Jade2DGameEngine/src/Components/ProjectileEmitterComponent.h
+++ Jade2DGameEngine/src/Components/ProjectileEmitterComponent.h
@@ -2,9 +2,27 @@
 #define PROJECTILEEMITTERCOMPONENT_H
 
 
+#include <glm/glm.hpp>
+
+
 struct ProjectileEmitterComponent {
 
-    
+    glm::vec2 projectileVelocity;
+    int repeatFrequency;
+    int projectileDuration;
+    int hitPercentDamage;
+    bool isFriendly;
+    int lastEmissionTime;
+
+    ProjectileEmitterComponent( glm::vec2 projectileVelocit = glm::vec2( 0 ), int repeatFrequency = 0, int projectileDuration = 10000, int hitPercentDamage = 10, bool isFriendly = false ) {
+
+        this->glm::vec2 projectileVelocity = projectileVelocity;
+        this->repeatFrequency = repeatFrequency;
+        this->projectileDuration = projectileDuration;
+        this->hitPercentDamage = hitPercentDamage;
+        this->isFriendly = isFriendly;
+        this->lastEmissionTime = SDL_GetTicks();
+    }
 };
 
 
