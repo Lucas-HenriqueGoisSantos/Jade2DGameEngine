b50185835b44e4e812503d3472fbd4913542c7fe
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-04 00:12:28 -0300

    small fix

--- Jade2DGameEngine/src/Systems/ProjectileEmitSystem.h
+++ Jade2DGameEngine/src/Systems/ProjectileEmitSystem.h
@@ -29,8 +29,17 @@ public:
 		
 			if ( SDL_GetTicks() - projectileEmitter.lastEmissionTime > projectileEmitter.repeatFrequency ) {
 				
+				glm::vec2 projectilePosition = transform.position;
+				if ( entity.HasComponent<SpriteComponent>() ) {
+
+					const auto sprite = entity.GetComponent<SpriteComponent>();
+					
+					projectilePosition.x += ( transform.scale.x * sprite.width / 2 );
+					projectilePosition.y += ( transform.scale.y * sprite.height / 2 );
+				}
+
 				Entity projectile  = registry->CreateEntity();
-				projectile.AddComponent<TransformComponent>( transform.position, glm::vec2( 1.0, 1.0 ), 0.0 );
+				projectile.AddComponent<TransformComponent>( projectilePosition, glm::vec2( 1.0, 1.0 ), 0.0 );
 				projectile.AddComponent<RigidBodyComponent>( projectileEmitter.projectileVelocity );
 				projectile.AddComponent<SpriteComponent>( "bullet-image", 4, 4, 4 );
 				projectile.AddComponent<BoxColliderComponent>( 4, 4 );
