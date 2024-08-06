bbdcbd2a2a1fa7a3a2fc32d782c0379c4c43e55e
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-05 22:36:51 -0300

    Implementing the tag and group prototypes in the Entity class

--- Jade2DGameEngine/src/ECS/ECS.cpp
+++ Jade2DGameEngine/src/ECS/ECS.cpp
@@ -20,6 +20,27 @@ void Entity::Kill() {
 }
 
 
+void Entity::Tag( const std::string& tag ) {
+
+	registry->TagEntity( *this, tag );
+}
+
+bool Entity::HasTag( const std::string& tag ) const {
+
+	return registry->EntityHasTag( *this, tag );
+}
+
+void Entity::Group( const std::string& group ) {
+
+	registry->GroupEntity( *this, group );
+}
+
+bool Entity::BelongsToGroup( const std::string& group ) const {
+
+	return registry->EntityBelongsToGroup( *this, group );
+}
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // System
 ///////////////////////////////////////////////////////////////////////////////////////////////
