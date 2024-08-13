7e3a45b63c395a2ef7084b928804666304f97a68
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-08-13 18:39:41 -0300

    Prototyping font sopport in the assetstore class

--- Jade2DGameEngine/src/AssetStore/AssetStore.h
+++ Jade2DGameEngine/src/AssetStore/AssetStore.h
@@ -4,12 +4,14 @@
 #include <map>
 #include <string>
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 
 class AssetStore {
 
 private:
 	std::map<std::string, SDL_Texture*> textures;
+	std::map<std::string, TTF_Font*> fonts;
 
 public:
 	AssetStore();
@@ -18,6 +20,9 @@ public:
 	void ClearAssets();
 	void AddTexture( SDL_Renderer* renderer, const std::string& assetId, const std::string& filePath );
 	SDL_Texture* GetTexture( const std::string& assetId );
+
+	void AddFont( const std::string& assetId, const std::string& filePath, int fontSize );
+	TTF_Font* GetFont( const std::string& assetId );
 };
 
 
