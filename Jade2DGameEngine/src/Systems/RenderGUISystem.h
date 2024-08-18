025a42b860e823f42a63657218847fcc294ba571
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-18 18:16:45 -0300

    Implementing a test GUI

--- Jade2DGameEngine/src/Systems/RenderGUISystem.h
+++ Jade2DGameEngine/src/Systems/RenderGUISystem.h
@@ -3,18 +3,24 @@
 
 
 #include "../ECS/ECS.h"
+#include <imgui/imgui.h>
+#include <imgui/imgui_sdl.h>
+#include <imgui/imgui_impl_sdl.h>
 
 
 class RenderGUISystem: public System {
 
-	RenderGUISystem() {
-
-
-	}
+public:
+	RenderGUISystem() = default;
 
 	void Update() {
+		
+		ImGui::NewFrame();
 
+		ImGui::Begin( "Test" );
 
+		ImGui::Render();
+		ImGuiSDL::Render( ImGui::GetDrawData() );
 	}
 };
 
