db70961335007451a2691e36fcb37d10f143975f
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-18 13:32:53 -0300

    Crearing RenderGUISystem file

---
+++ Jade2DGameEngine/src/Systems/RenderGUISystem.h
@@ -0,0 +1,22 @@
+#ifndef RENDERGUISYSTEM_H
+#define RENDERGUISYSTEM_H
+
+
+#include "../ECS/ECS.h"
+
+
+class RenderGUISystem: public System {
+
+	RenderGUISystem() {
+
+
+	}
+
+	void Update() {
+
+
+	}
+};
+
+
+#endif // !RENDERGUISYSTEM_H
