718e4ef7c2ceec97d92f09c92c77a3c210451e05
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-20 10:16:11 -0300

    Creating a more complete GUI for spawning enemies

--- Jade2DGameEngine/src/Systems/RenderGUISystem.h
+++ Jade2DGameEngine/src/Systems/RenderGUISystem.h
@@ -27,22 +27,79 @@ public:
 		// ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoNav;
 		if ( ImGui::Begin( "Spawn Enemies"/*, NULL, windowFlags*/ ) ) {
 
-			static int enemyXPos = 0;
-			static int enemyYPos = 0;
+			static int posX = 0;
+			static int posY = 0;
+			static int scaleX = 1;
+			static int scaleY = 1;
+			static int velX = 0;
+			static int velY = 0;
+			static int health = 100;
+			static float rotation = 0.0;
+			static float projAngle = 0.0;
+			static float projSpeed = 100.0;
+			static int projRepeat = 10;
+			static int projDuration = 10;
+			const char* sprites[] = { "tank-image", "truck-image" };
+			static int seclectedSpriteIndex = 0;
 
-			ImGui::InputInt( "X position", &enemyXPos );
-			ImGui::InputInt( "Y position", &enemyYPos );
 
-			if ( ImGui::Button( "Create new enemy" ) ) {
+			if ( ImGui::CollapsingHeader( "Sprite", ImGuiTreeNodeFlags_DefaultOpen ) ) {
+				
+				ImGui::Combo( "Texture ID", &seclectedSpriteIndex, sprites, IM_ARRAYSIZE( sprites ) );
+			}
+			ImGui::Spacing();
+
+			if ( ImGui::CollapsingHeader( "Transform", ImGuiTreeNodeFlags_DefaultOpen ) ) {
+
+				ImGui::InputInt( "Position X", &posX );
+				ImGui::InputInt( "Position Y", &posY );
+				ImGui::SliderInt( "Scale X", &scaleX, 1, 10 );
+				ImGui::SliderInt( "Scale Y", &scaleY, 1, 10 );
+				ImGui::SliderAngle( "Rotation (deg)", &rotation, 0, 360 );
+			}
+			ImGui::Spacing();
+
+			if ( ImGui::CollapsingHeader( "Rigid Body", ImGuiTreeNodeFlags_DefaultOpen ) ) {
+
+				ImGui::InputInt( "Velocity X", &velX );
+				ImGui::InputInt( "Velocity Y", &velY );
+			}
+			ImGui::Spacing();
+
+			if ( ImGui::CollapsingHeader( "Projectile Emitter", ImGuiTreeNodeFlags_DefaultOpen ) ) {
+
+				ImGui::SliderAngle( "Angle (deg)", &projAngle, 0, 360 );
+				ImGui::SliderFloat( "Speed (px/sec)", &projSpeed, 10, 500 );
+				ImGui::InputInt( "Repeat (sec)", &projRepeat );
+				ImGui::InputInt( "Duration (sec)", &projDuration );
+			}
+			ImGui::Spacing();
+
+			if ( ImGui::CollapsingHeader( "Health", ImGuiTreeNodeFlags_DefaultOpen ) ) {
+
+				ImGui::SliderInt( "%", &health, 0, 100 );
+			}
+			ImGui::Spacing();
+
+			ImGui::Separator();
+			ImGui::Spacing();
+
+			if ( ImGui::Button( "Spawn new enemy" ) ) {
 
 				Entity enemy = registry->CreateEntity();
 				enemy.Group( "enemies" );
-				enemy.AddComponent<TransformComponent>( glm::vec2( enemyXPos, enemyYPos ), glm::vec2( 1.0, 1.0 ), 0.0 );
-				enemy.AddComponent<RigidBodyComponent>( glm::vec2( 0.0, 0.0 ) );
-				enemy.AddComponent<SpriteComponent>( "tank-image", 32, 32, 1 );
-				enemy.AddComponent<BoxColliderComponent>( 32, 32 );
-				enemy.AddComponent<ProjectileEmitterComponent>( glm::vec2( 0.0, 50.0 ), 1000, 1000, 10, false );
-				enemy.AddComponent<HealthComponent>( 100 );
+				enemy.AddComponent<TransformComponent>( glm::vec2( posX, posY ), glm::vec2( scaleX, scaleY ), glm::degrees( rotation ) );
+				enemy.AddComponent<RigidBodyComponent>( glm::vec2( velX, velY ) );
+				enemy.AddComponent<SpriteComponent>( sprites[seclectedSpriteIndex], 32, 32, 2 );
+				enemy.AddComponent<BoxColliderComponent>( 32, 32, glm::vec2( 5, 5 ) );
+				double projVelX = cos( projAngle ) * projSpeed;
+				double projVelY = sin( projAngle ) * projSpeed;
+				enemy.AddComponent<ProjectileEmitterComponent>( glm::vec2( projVelX, projVelY ), projRepeat * 1000, projDuration * 1000, 10, false );
+				enemy.AddComponent<HealthComponent>( health );
+
+				posX = posY = scaleX = scaleY = rotation = projAngle = 0;
+				projRepeat = projDuration = 10;
+				projSpeed = health = 100;
 			}
 		}
 		ImGui::End();
