5334510ecd6481f56189642b20959136fd6c94aa
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-08-23 19:21:41 -0300

    Reorganizing the Logger

--- Jade2DGameEngine/src/Logger/Logger.h
+++ Jade2DGameEngine/src/Logger/Logger.h
@@ -5,17 +5,20 @@
 #include <string>
 
 enum LogType {
+
     LOG_INFO,
     LOG_WARNING,
     LOG_ERROR
 };
 
 struct LogEntry {
+
     LogType type;
     std::string message;
 };
 
 class Logger {
+    
     public:
         static std::vector<LogEntry> messages;
         static void Log(const std::string& message);
