dd88a302033418d3c84a1293efa767672464b1a5
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-08-24 18:50:54 -0300

    Reorganizing Game.h and Game.cpp

--- Jade2DGameEngine/src/Game/Game.cpp
+++ Jade2DGameEngine/src/Game/Game.cpp
@@ -39,52 +39,59 @@ int Game::mapWidth;
 int Game::mapHeight;
 
 Game::Game() {
+
     isRunning = false;
     isDebug = false;
     registry = std::make_unique<Registry>();
     assetStore = std::make_unique<AssetStore>();
     eventBus = std::make_unique<EventBus>();
-    Logger::Log("Game constructor called!");
+    Logger::Log( "Game constructor called!" );
 }
 
 Game::~Game() {
-    Logger::Log("Game destructor called!");   
+
+    Logger::Log( "Game destructor called!" );   
 }
 
 void Game::Initialize() {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        Logger::Err("Error initializing SDL.");
+
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) != 0 ) {
+
+        Logger::Err( "Error initializing SDL." );
         return;
     }
-    if (TTF_Init() != 0) {
-        Logger::Err("Error initializing SDL TTF.");
+    if ( TTF_Init() != 0 ) {
+
+        Logger::Err( "Error initializing SDL TTF." );
         return;
     }
     SDL_DisplayMode displayMode;
-    SDL_GetCurrentDisplayMode(0, &displayMode);
+    SDL_GetCurrentDisplayMode( 0, &displayMode );
     windowWidth = displayMode.w;
     windowHeight = displayMode.h;
-    window = SDL_CreateWindow(
+    window = SDL_CreateWindow( 
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         windowWidth,
         windowHeight,
         SDL_WINDOW_BORDERLESS
-    );
-    if (!window) {
-        Logger::Err("Error creating SDL window.");
+     );
+    if ( !window ) {
+
+        Logger::Err( "Error creating SDL window." );
         return;
     }
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        Logger::Err("Error creating SDL renderer.");
+    renderer = SDL_CreateRenderer( window, -1, 0 );
+    if ( !renderer ) {
+
+        Logger::Err( "Error creating SDL renderer." );
         return;
     }
 
     // Initialize the ImGui context
     ImGui::CreateContext();
-    ImGuiSDL::Initialize(renderer, windowWidth, windowHeight);
+    ImGuiSDL::Initialize( renderer, windowWidth, windowHeight );
 
     // Initialize the camera view with the entire screen area
     camera.x = 0;
@@ -92,41 +99,48 @@ void Game::Initialize() {
     camera.w = windowWidth;
     camera.h = windowHeight;
 
-    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+    SDL_SetWindowFullscreen( window, SDL_WINDOW_FULLSCREEN );
     isRunning = true;
 }
 
 void Game::ProcessInput() {
+
     SDL_Event sdlEvent;
-    while (SDL_PollEvent(&sdlEvent)) {
+    while ( SDL_PollEvent( &sdlEvent ) ) {
+
         // ImGui SDL input
-        ImGui_ImplSDL2_ProcessEvent(&sdlEvent);
+        ImGui_ImplSDL2_ProcessEvent( &sdlEvent );
         ImGuiIO& io = ImGui::GetIO();
         int mouseX, mouseY;
-        const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
-        io.MousePos = ImVec2(mouseX, mouseY);
-        io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
-        io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+        const int buttons = SDL_GetMouseState( &mouseX, &mouseY );
+        io.MousePos = ImVec2( mouseX, mouseY );
+        io.MouseDown[0] = buttons & SDL_BUTTON( SDL_BUTTON_LEFT );
+        io.MouseDown[1] = buttons & SDL_BUTTON( SDL_BUTTON_RIGHT );
 
-        // Handle core SDL events (close window, key pressed, etc.)
-        switch (sdlEvent.type) {
+        // Handle core SDL events ( close window, key pressed, etc. )
+        switch ( sdlEvent.type ) {
+
             case SDL_QUIT:
                 isRunning = false;
                 break;
+                
             case SDL_KEYDOWN:
-                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                if ( sdlEvent.key.keysym.sym == SDLK_ESCAPE ) {
+
                     isRunning = false;
                 }
-                if (sdlEvent.key.keysym.sym == SDLK_F1) {
+                if ( sdlEvent.key.keysym.sym == SDLK_F1 ) {
+
                     isDebug = !isDebug;
                 }
-                eventBus->EmitEvent<KeyPressedEvent>(sdlEvent.key.keysym.sym);
+                eventBus->EmitEvent<KeyPressedEvent>( sdlEvent.key.keysym.sym );
                 break;
         }
     }
 }
 
-void Game::LoadLevel(int level) {
+void Game::LoadLevel( int level ) {
+
     // Add the sytems that need to be processed in our game
     registry->AddSystem<MovementSystem>();
     registry->AddSystem<RenderSystem>();
@@ -143,15 +157,15 @@ void Game::LoadLevel(int level) {
     registry->AddSystem<RenderGUISystem>();
 
     // Adding assets to the asset store
-    assetStore->AddTexture(renderer, "tank-image", "./assets/images/tank-panther-right.png");
-    assetStore->AddTexture(renderer, "truck-image", "./assets/images/truck-ford-right.png");
-    assetStore->AddTexture(renderer, "chopper-image", "./assets/images/chopper-spritesheet.png");
-    assetStore->AddTexture(renderer, "radar-image", "./assets/images/radar.png");
-    assetStore->AddTexture(renderer, "tilemap-image", "./assets/tilemaps/jungle.png");
-    assetStore->AddTexture(renderer, "bullet-image", "./assets/images/bullet.png");
-    assetStore->AddFont("charriot-font-20", "./assets/fonts/charriot.ttf", 20);
-    assetStore->AddFont("pico8-font-5", "./assets/fonts/pico8.ttf", 5);
-    assetStore->AddFont("pico8-font-10", "./assets/fonts/pico8.ttf", 10);
+    assetStore->AddTexture( renderer, "tank-image", "./assets/images/tank-panther-right.png" );
+    assetStore->AddTexture( renderer, "truck-image", "./assets/images/truck-ford-right.png" );
+    assetStore->AddTexture( renderer, "chopper-image", "./assets/images/chopper-spritesheet.png" );
+    assetStore->AddTexture( renderer, "radar-image", "./assets/images/radar.png" );
+    assetStore->AddTexture( renderer, "tilemap-image", "./assets/tilemaps/jungle.png" );
+    assetStore->AddTexture( renderer, "bullet-image", "./assets/images/bullet.png" );
+    assetStore->AddFont( "charriot-font-20", "./assets/fonts/charriot.ttf", 20 );
+    assetStore->AddFont( "pico8-font-5", "./assets/fonts/pico8.ttf", 5 );
+    assetStore->AddFont( "pico8-font-10", "./assets/fonts/pico8.ttf", 10 );
 
     // Load the tilemap
     int tileSize = 32;
@@ -160,21 +174,23 @@ void Game::LoadLevel(int level) {
     int mapNumRows = 20;
 
     std::fstream mapFile;
-    mapFile.open("./assets/tilemaps/jungle.map");
+    mapFile.open( "./assets/tilemaps/jungle.map" );
 
-    for (int y = 0; y < mapNumRows; y++) {
-        for (int x = 0; x < mapNumCols; x++) {
+    for ( int y = 0; y < mapNumRows; y++ ) {
+
+        for ( int x = 0; x < mapNumCols; x++ ) {
+
             char ch;
-            mapFile.get(ch);
-            int srcRectY = std::atoi(&ch) * tileSize;
-            mapFile.get(ch);
-            int srcRectX = std::atoi(&ch) * tileSize;
+            mapFile.get( ch );
+            int srcRectY = std::atoi( &ch ) * tileSize;
+            mapFile.get( ch );
+            int srcRectX = std::atoi( &ch ) * tileSize;
             mapFile.ignore();
 
             Entity tile = registry->CreateEntity();
-            tile.Group("tiles");
-            tile.AddComponent<TransformComponent>(glm::vec2(x * (tileScale * tileSize), y * (tileScale * tileSize)), glm::vec2(tileScale, tileScale), 0.0);
-            tile.AddComponent<SpriteComponent>("tilemap-image", tileSize, tileSize, 0, false, srcRectX, srcRectY);
+            tile.Group( "tiles" );
+            tile.AddComponent<TransformComponent>( glm::vec2( x * ( tileScale * tileSize ), y * ( tileScale * tileSize ) ), glm::vec2( tileScale, tileScale ), 0.0 );
+            tile.AddComponent<SpriteComponent>( "tilemap-image", tileSize, tileSize, 0, false, srcRectX, srcRectY );
         }
     }
     mapFile.close();
@@ -183,59 +199,62 @@ void Game::LoadLevel(int level) {
 
     // Create an entity
     Entity chopper = registry->CreateEntity();
-    chopper.Tag("player");
-    chopper.AddComponent<TransformComponent>(glm::vec2(240.0, 110.0), glm::vec2(1.0, 1.0), 0.0);
-    chopper.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
-    chopper.AddComponent<SpriteComponent>("chopper-image", 32, 32, 1);
-    chopper.AddComponent<AnimationComponent>(2, 15, true);
-    chopper.AddComponent<BoxColliderComponent>(32, 25, glm::vec2(0, 5));
-    chopper.AddComponent<ProjectileEmitterComponent>(glm::vec2(150.0, 150.0), 0, 10000, 10, true);
-    chopper.AddComponent<KeyboardControlledComponent>(glm::vec2(0, -50), glm::vec2(50, 0), glm::vec2(0, 50), glm::vec2(-50, 0));
+    chopper.Tag( "player" );
+    chopper.AddComponent<TransformComponent>( glm::vec2( 240.0, 110.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
+    chopper.AddComponent<RigidBodyComponent>( glm::vec2( 0.0, 0.0 ) );
+    chopper.AddComponent<SpriteComponent>( "chopper-image", 32, 32, 1 );
+    chopper.AddComponent<AnimationComponent>( 2, 15, true );
+    chopper.AddComponent<BoxColliderComponent>( 32, 25, glm::vec2( 0, 5 ) );
+    chopper.AddComponent<ProjectileEmitterComponent>( glm::vec2( 150.0, 150.0 ), 0, 10000, 10, true );
+    chopper.AddComponent<KeyboardControlledComponent>( glm::vec2( 0, -50 ), glm::vec2( 50, 0 ), glm::vec2( 0, 50 ), glm::vec2( -50, 0 ) );
     chopper.AddComponent<CameraFollowComponent>();
-    chopper.AddComponent<HealthComponent>(100);
+    chopper.AddComponent<HealthComponent>( 100 );
     
     Entity radar = registry->CreateEntity();
-    radar.AddComponent<TransformComponent>(glm::vec2(windowWidth - 74, 10.0), glm::vec2(1.0, 1.0), 0.0);
-    radar.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
-    radar.AddComponent<SpriteComponent>("radar-image", 64, 64, 1, true);
-    radar.AddComponent<AnimationComponent>(8, 5, true);
+    radar.AddComponent<TransformComponent>( glm::vec2( windowWidth - 74, 10.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
+    radar.AddComponent<RigidBodyComponent>( glm::vec2( 0.0, 0.0 ) );
+    radar.AddComponent<SpriteComponent>( "radar-image", 64, 64, 1, true );
+    radar.AddComponent<AnimationComponent>( 8, 5, true );
     
     Entity tank = registry->CreateEntity();
-    tank.Group("enemies");
-    tank.AddComponent<TransformComponent>(glm::vec2(500.0, 500.0), glm::vec2(1.0, 1.0), 0.0);
-    tank.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
-    tank.AddComponent<SpriteComponent>("tank-image", 32, 32, 1);
-    tank.AddComponent<BoxColliderComponent>(25, 18, glm::vec2(5, 7));
-    tank.AddComponent<ProjectileEmitterComponent>(glm::vec2(100.0, 0.0), 3000, 5000, 10, false);
-    tank.AddComponent<HealthComponent>(100);
+    tank.Group( "enemies" );
+    tank.AddComponent<TransformComponent>( glm::vec2( 500.0, 500.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
+    tank.AddComponent<RigidBodyComponent>( glm::vec2( 0.0, 0.0 ) );
+    tank.AddComponent<SpriteComponent>( "tank-image", 32, 32, 1 );
+    tank.AddComponent<BoxColliderComponent>( 25, 18, glm::vec2( 5, 7 ) );
+    tank.AddComponent<ProjectileEmitterComponent>( glm::vec2( 100.0, 0.0 ), 3000, 5000, 10, false );
+    tank.AddComponent<HealthComponent>( 100 );
 
     Entity truck = registry->CreateEntity();
-    truck.Group("enemies");
-    truck.AddComponent<TransformComponent>(glm::vec2(120.0, 500.0), glm::vec2(1.0, 1.0), 0.0);
-    truck.AddComponent<RigidBodyComponent>(glm::vec2(0.0, 0.0));
-    truck.AddComponent<SpriteComponent>("truck-image", 32, 32, 2);
-    truck.AddComponent<BoxColliderComponent>(25, 20, glm::vec2(5, 5));
-    truck.AddComponent<ProjectileEmitterComponent>(glm::vec2(0.0, -100.0), 2000, 5000, 10, false);
-    truck.AddComponent<HealthComponent>(100);
+    truck.Group( "enemies" );
+    truck.AddComponent<TransformComponent>( glm::vec2( 120.0, 500.0 ), glm::vec2( 1.0, 1.0 ), 0.0 );
+    truck.AddComponent<RigidBodyComponent>( glm::vec2( 0.0, 0.0 ) );
+    truck.AddComponent<SpriteComponent>( "truck-image", 32, 32, 2 );
+    truck.AddComponent<BoxColliderComponent>( 25, 20, glm::vec2( 5, 5 ) );
+    truck.AddComponent<ProjectileEmitterComponent>( glm::vec2( 0.0, -100.0 ), 2000, 5000, 10, false );
+    truck.AddComponent<HealthComponent>( 100 );
 
     Entity label = registry->CreateEntity();
-    SDL_Color green = {0, 255, 0};
-    label.AddComponent<TextLabelComponent>(glm::vec2(windowWidth / 2 - 40, 10), "CHOPPER 1.0", "pico8-font-10", green, true);
+    SDL_Color green = { 0, 255, 0 };
+    label.AddComponent<TextLabelComponent>( glm::vec2( windowWidth / 2 - 40, 10 ), "CHOPPER 1.0", "pico8-font-10", green, true );
 }
 
 void Game::Setup() {
-    LoadLevel(1);
+
+    LoadLevel( 1 );
 }
 
 void Game::Update() {
+
     // If we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
-    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
-    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
-        SDL_Delay(timeToWait);
+    int timeToWait = MILLISECS_PER_FRAME - ( SDL_GetTicks() - millisecsPreviousFrame );
+    if ( timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME ) {
+
+        SDL_Delay( timeToWait );
     }
 
     // The difference in ticks since the last frame, converted to seconds
-    double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+    double deltaTime = ( SDL_GetTicks() - millisecsPreviousFrame ) / 1000.0;
 
     // Store the "previous" frame time
     millisecsPreviousFrame = SDL_GetTicks();
@@ -244,41 +263,45 @@ void Game::Update() {
     eventBus->Reset();
 
     // Perform the subscription of the events for all systems
-    registry->GetSystem<DamageSystem>().SubscribeToEvents(eventBus);
-    registry->GetSystem<KeyboardControlSystem>().SubscribeToEvents(eventBus);
-    registry->GetSystem<ProjectileEmitSystem>().SubscribeToEvents(eventBus);
+    registry->GetSystem<DamageSystem>().SubscribeToEvents( eventBus );
+    registry->GetSystem<KeyboardControlSystem>().SubscribeToEvents( eventBus );
+    registry->GetSystem<ProjectileEmitSystem>().SubscribeToEvents( eventBus );
 
     // Update the registry to process the entities that are waiting to be created/deleted
     registry->Update();
     
     // Invoke all the systems that need to update 
-    registry->GetSystem<MovementSystem>().Update(deltaTime);
+    registry->GetSystem<MovementSystem>().Update( deltaTime );
     registry->GetSystem<AnimationSystem>().Update();
-    registry->GetSystem<CollisionSystem>().Update(eventBus);
-    registry->GetSystem<ProjectileEmitSystem>().Update(registry);
-    registry->GetSystem<CameraMovementSystem>().Update(camera);
+    registry->GetSystem<CollisionSystem>().Update( eventBus );
+    registry->GetSystem<ProjectileEmitSystem>().Update( registry );
+    registry->GetSystem<CameraMovementSystem>().Update( camera );
     registry->GetSystem<ProjectileLifecycleSystem>().Update();
 }
 
 void Game::Render() {
-    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
-    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor( renderer, 21, 21, 21, 255 );
+    SDL_RenderClear( renderer );
 
     // Invoke all the systems that need to render 
-    registry->GetSystem<RenderSystem>().Update(renderer, assetStore, camera);
-    registry->GetSystem<RenderTextSystem>().Update(renderer, assetStore, camera);
-    registry->GetSystem<RenderHealthBarSystem>().Update(renderer, assetStore, camera);
-    if (isDebug) {
-        registry->GetSystem<RenderColliderSystem>().Update(renderer, camera);
-        registry->GetSystem<RenderGUISystem>().Update(registry, camera);
+    registry->GetSystem<RenderSystem>().Update( renderer, assetStore, camera );
+    registry->GetSystem<RenderTextSystem>().Update( renderer, assetStore, camera );
+    registry->GetSystem<RenderHealthBarSystem>().Update( renderer, assetStore, camera );
+    if ( isDebug ) {
+
+        registry->GetSystem<RenderColliderSystem>().Update( renderer, camera );
+        registry->GetSystem<RenderGUISystem>().Update( registry, camera );
     }
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent( renderer );
 }
 
 void Game::Run() {
+
     Setup();
-    while (isRunning) {
+    while ( isRunning ) {
+
         ProcessInput();
         Update();
         Render();
@@ -286,9 +309,10 @@ void Game::Run() {
 }
 
 void Game::Destroy() {
+
     ImGuiSDL::Deinitialize();
     ImGui::DestroyContext();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer( renderer );
+    SDL_DestroyWindow( window );
     SDL_Quit();
 }
--- Jade2DGameEngine/src/Game/Game.h
+++ Jade2DGameEngine/src/Game/Game.h
@@ -10,6 +10,7 @@ const int FPS = 60;
 const int MILLISECS_PER_FRAME = 1000 / FPS;
 
 class Game {
+
     private:
         bool isRunning;
         bool isDebug;
@@ -28,7 +29,7 @@ class Game {
         void Initialize();
         void Run();
         void Setup();
-        void LoadLevel(int level);
+        void LoadLevel( int level );
         void ProcessInput();
         void Update();
         void Render();
