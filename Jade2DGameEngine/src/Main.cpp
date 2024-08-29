bb21d93764787f490c60ce24d7cce674f4e9d389
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-08-29 14:17:04 -0300

    Comenting TestLua

--- Jade2DGameEngine/src/Main.cpp
+++ Jade2DGameEngine/src/Main.cpp
@@ -5,11 +5,14 @@
 
 void TestLua() {
 
+    // Creating the Lua state in with Sol
     sol::state lua;
+    // Importing libs for Lua, all base Sol libs
     lua.open_libraries( sol::lib::base );
-
+    // Importing a Lua Script
     lua.script_file( "./assets/scripts/hello.lua" );
 
+    // Importing Lua values to C++
     int someVarInsideCpp = lua["some_variable"];
     std::cout << "The value os some_variable in C++ is " << someVarInsideCpp << std::endl;
 }
