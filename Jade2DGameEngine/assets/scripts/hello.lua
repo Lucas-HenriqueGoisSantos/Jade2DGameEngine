6ab1471cc38058f77bd02df7c38d0cdcb5570bea
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-28 23:14:47 -0300

    Testing the Sol library

---
+++ Jade2DGameEngine/assets/scripts/hello.lua
@@ -0,0 +1,9 @@
+-- This is comment
+-- This is a Lua Global Variable
+
+some_variable = 7 * 6
+
+if some_variable == 42 then
+    
+    print( "The value of the variable is "..some_variable )
+end
--- Jade2DGameEngine/src/Main.cpp
+++ Jade2DGameEngine/src/Main.cpp
@@ -1,12 +1,29 @@
 #include "./Game/Game.h"
+#include <sol/sol.hpp>
+#include <iostream>
+
+
+void TestLua() {
+
+    sol::state lua;
+    lua.open_libraries( sol::lib::base );
+
+    lua.script_file( "./assets/scripts/hello.lua" );
+
+    int someVarInsideCpp = lua["some_variable"];
+    std::cout << "The value os some_variable in C++ is " << someVarInsideCpp << std::endl;
+}
+
 
 int main(int argc, char* argv[]) {
     
-    Game game;
+    //Game game;
 
-    game.Initialize();
-    game.Run();
-    game.Destroy();
+    //game.Initialize();
+    //game.Run();
+    //game.Destroy();
+
+    TestLua();
 
     return 0;
 }
