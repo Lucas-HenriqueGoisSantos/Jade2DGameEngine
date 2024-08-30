36d34c46508c67a145c1a754f29983ef55acea94
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-30 10:21:33 -0300

    Passing LoadLevel, from Game.cpp, to LevelLoader.cpp

--- Jade2DGameEngine/src/Game/LevelLoader.h
+++ Jade2DGameEngine/src/Game/LevelLoader.h
@@ -2,14 +2,18 @@
 #define LEVELLOADER_H
 
 
+#include "../ECS/ECS.h"
+#include "../AssetStore/AssetStore.h"
+#include <SDL.h>
+#include <memory>
+
+
 class LevelLoader {
 
 	public:
 		LevelLoader();
 		~LevelLoader();
 
-		void LoadLevel( int level ) {
-
-		}
+		void LoadLevel( const std::unique_ptr<Registry>& registry, const std::unique_ptr<AssetStore>& assetStore, SDL_Renderer* renderer, int level );
 };
 #endif // !LEVELLOADER_H
