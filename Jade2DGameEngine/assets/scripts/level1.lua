bb077863babaaf81b0322270bade6322c6808b3f
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-08-30 08:59:16 -0300

    Reorganizing the level1 file

--- Jade2DGameEngine/assets/scripts/level1.lua
+++ Jade2DGameEngine/assets/scripts/level1.lua
@@ -1,9 +1,11 @@
 -- Define a table with the values of the first level
 Level = {
+
     ----------------------------------------------------
     -- Table to define the list of assets
     ----------------------------------------------------
     assets = {
+
         [0] =
         { type = "texture", id = "tilemap-texture", file = "./assets/tilemaps/jungle.png" },
         { type = "texture", id = "chopper-texture", file = "./assets/images/chopper-green-spritesheet.png" },
@@ -37,15 +39,19 @@ Level = {
             -- Player
             tag = "player",
             components = {
+
                 transform = {
+
                     position = { x = 242, y = 110 },
                     scale = { x = 1.0, y = 1.0 },
                     rotation = 0.0, -- degrees
                 },
                 rigidbody = {
+
                     velocity = { x = 0.0, y = 0.0 }
                 },
                 sprite = {
+
                     texture_asset_id = "chopper-texture",
                     width = 32,
                     height = 32,
@@ -55,18 +61,22 @@ Level = {
                     src_rect_y = 0
                 },
                 animation = {
+
                     num_frames = 2,
                     speed_rate = 10 -- fps
                 },
                 boxcollider = {
+
                     width = 32,
                     height = 25,
                     offset = { x = 0, y = 5 }
                 },
                 health = {
+
                     health_percentage = 100
                 },
                 projectile_emitter = {
+
                     projectile_velocity = { x = 200, y = 200 },
                     projectile_duration = 10, -- seconds
                     repeat_frequency = 0, -- seconds
@@ -74,12 +84,14 @@ Level = {
                     friendly = true
                 },
                 keyboard_controller = {
+
                     up_velocity = { x = 0, y = -50 },
                     right_velocity = { x = 50, y = 0 },
                     down_velocity = { x = 0, y = 50 },
                     left_velocity = { x = -50, y = 0 }
                 },
                 camera_follow = {
+
                     follow = true
                 }
             }
@@ -89,26 +101,32 @@ Level = {
             -- Tank
             group = "enemies",
             components = {
+
                 transform = {
+
                     position = { x = 200, y = 497 },
                     scale = { x = 1.0, y = 1.0 },
                     rotation = 0.0, -- degrees
                 },
                 sprite = {
+
                     texture_asset_id = "tank-texture",
                     width = 32,
                     height = 32,
                     z_index = 2
                 },
                 boxcollider = {
+
                     width = 25,
                     height = 18,
                     offset = { x = 0, y = 7 }
                 },
                 health = {
+
                     health_percentage = 100
                 },
                 projectile_emitter = {
+
                     projectile_velocity = { x = 100, y = 0 },
                     projectile_duration = 2, -- seconds
                     repeat_frequency = 1, -- seconds
