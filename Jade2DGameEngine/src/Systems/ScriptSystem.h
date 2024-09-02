01d8dd1145c5dc8d27057a5a9448bcb4e5832306
Lucas-HenriqueGoisSantos <89606089+Lucas-HenriqueGoisSantos@users.noreply.github.com>
2024-09-02 19:26:01 -0300

    Implemented a Script System

---
+++ Jade2DGameEngine/src/Systems/ScriptSystem.h
@@ -0,0 +1,27 @@
+#ifndef SCRIPTSYSTEM_H
+#define SCRIPTSYSTEM_H
+
+
+#include "../ECS/ECS.h"
+#include "../Components/ScriptComponent.h"
+
+
+class ScriptSystem: public System {
+
+    public:
+        ScriptSystem() {
+
+            RequireComponent<ScriptComponent>();
+        }
+
+        void Update() {
+
+            for ( auto entity: GetSystemEntities() ) {
+
+                const auto script = entity.GetComponent<ScriptComponent>();
+                script.func();
+            }
+        }
+};
+
+#endif
