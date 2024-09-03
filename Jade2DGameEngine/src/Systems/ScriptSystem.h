ee637b07a62e5f59d9e8da5aa02e237dc8b8a53b
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-03 15:25:10 -0300

    Reorganizing code

--- Jade2DGameEngine/src/Systems/ScriptSystem.h
+++ Jade2DGameEngine/src/Systems/ScriptSystem.h
@@ -10,78 +10,96 @@
 #include "../Components/ProjectileEmitterComponent.h"
 #include <tuple>
 
-std::tuple<double, double> GetEntityPosition(Entity entity) {
-    if (entity.HasComponent<TransformComponent>()) {
+std::tuple<double, double> GetEntityPosition( Entity entity ) {
+
+    if ( entity.HasComponent<TransformComponent>() ) {
+
         const auto transform = entity.GetComponent<TransformComponent>();
-        return std::make_tuple(transform.position.x, transform.position.y);
-    }
-    else {
-        Logger::Err("Trying to get the position of an entity that has no transform component");
-        return std::make_tuple(0.0, 0.0);
+
+        return std::make_tuple( transform.position.x, transform.position.y );
+    } else {
+
+        Logger::Err( "Trying to get the position of an entity that has no transform component" );
+        
+        return std::make_tuple( 0.0, 0.0 );
     }
 }
 
-std::tuple<double, double> GetEntityVelocity(Entity entity) {
-    if (entity.HasComponent<RigidBodyComponent>()) {
+std::tuple<double, double> GetEntityVelocity( Entity entity ) {
+
+    if ( entity.HasComponent<RigidBodyComponent>() ) {
+
         const auto rigidbody = entity.GetComponent<RigidBodyComponent>();
-        return std::make_tuple(rigidbody.velocity.x, rigidbody.velocity.y);
-    }
-    else {
-        Logger::Err("Trying to get the velocity of an entity that has no rigidbody component");
-        return std::make_tuple(0.0, 0.0);
+        
+        return std::make_tuple( rigidbody.velocity.x, rigidbody.velocity.y );
+    } else {
+
+        Logger::Err( "Trying to get the velocity of an entity that has no rigidbody component" );
+        
+        return std::make_tuple( 0.0, 0.0 );
     }
 }
 
-void SetEntityPosition(Entity entity, double x, double y) {
-    if (entity.HasComponent<TransformComponent>()) {
+void SetEntityPosition( Entity entity, double x, double y ) {
+
+    if ( entity.HasComponent<TransformComponent>() ) {
+
         auto& transform = entity.GetComponent<TransformComponent>();
         transform.position.x = x;
         transform.position.y = y;
-    }
-    else {
-        Logger::Err("Trying to set the position of an entity that has no transform component");
+    } else {
+
+        Logger::Err( "Trying to set the position of an entity that has no transform component" );
     }
 }
 
-void SetEntityVelocity(Entity entity, double x, double y) {
-    if (entity.HasComponent<RigidBodyComponent>()) {
+void SetEntityVelocity( Entity entity, double x, double y ) {
+
+    if ( entity.HasComponent<RigidBodyComponent>() ) {
+
         auto& rigidbody = entity.GetComponent<RigidBodyComponent>();
         rigidbody.velocity.x = x;
         rigidbody.velocity.y = y;
-    }
-    else {
-        Logger::Err("Trying to set the velocity of an entity that has no rigidbody component");
+    } else {
+
+        Logger::Err( "Trying to set the velocity of an entity that has no rigidbody component" );
     }
 }
 
-void SetEntityRotation(Entity entity, double angle) {
-    if (entity.HasComponent<TransformComponent>()) {
+void SetEntityRotation( Entity entity, double angle ) {
+
+    if ( entity.HasComponent<TransformComponent>() ) {
+
         auto& transform = entity.GetComponent<TransformComponent>();
         transform.rotation = angle;
-    }
-    else {
-        Logger::Err("Trying to set the rotation of an entity that has no transform component");
+    } else {
+
+        Logger::Err( "Trying to set the rotation of an entity that has no transform component" );
     }
 }
 
-void SetEntityAnimationFrame(Entity entity, int frame) {
-    if (entity.HasComponent<AnimationComponent>()) {
+void SetEntityAnimationFrame( Entity entity, int frame ) {
+
+    if ( entity.HasComponent<AnimationComponent>() ) {
+
         auto& animation = entity.GetComponent<AnimationComponent>();
         animation.currentFrame = frame;
-    }
-    else {
-        Logger::Err("Trying to set the animation frame of an entity that has no animation component");
+    } else {
+
+        Logger::Err( "Trying to set the animation frame of an entity that has no animation component" );
     }
 }
 
-void SetProjectileVelocity(Entity entity, double x, double y) {
-    if (entity.HasComponent<ProjectileEmitterComponent>()) {
+void SetProjectileVelocity( Entity entity, double x, double y ) {
+
+    if ( entity.HasComponent<ProjectileEmitterComponent>() ) {
+
         auto& projectileEmitter = entity.GetComponent<ProjectileEmitterComponent>();
         projectileEmitter.projectileVelocity.x = x;
         projectileEmitter.projectileVelocity.y = y;
-    }
-    else {
-        Logger::Err("Trying to set the projectile velocity of an entity that has no projectile emitter component");
+    } else {
+
+        Logger::Err( "Trying to set the projectile velocity of an entity that has no projectile emitter component" );
     }
 }
 
@@ -105,13 +123,13 @@ class ScriptSystem: public System {
                 "belongs_to_group", &Entity::BelongsToGroup
             );
             // Create all the bindings between C++ and Lua functions
-            lua.set_function("get_position", GetEntityPosition);
-            lua.set_function("get_velocity", GetEntityVelocity);
-            lua.set_function("set_position", SetEntityPosition);
-            lua.set_function("set_velocity", SetEntityVelocity);
-            lua.set_function("set_rotation", SetEntityRotation);
-            lua.set_function("set_projectile_velocity", SetProjectileVelocity);
-            lua.set_function("set_animation_frame", SetEntityAnimationFrame);
+            lua.set_function( "get_position", GetEntityPosition );
+            lua.set_function( "get_velocity", GetEntityVelocity );
+            lua.set_function( "set_position", SetEntityPosition );
+            lua.set_function( "set_velocity", SetEntityVelocity );
+            lua.set_function( "set_rotation", SetEntityRotation );
+            lua.set_function( "set_projectile_velocity", SetProjectileVelocity );
+            lua.set_function( "set_animation_frame", SetEntityAnimationFrame );
         }
 
         void Update( double deltaTime, int ellapsedTime ) {
