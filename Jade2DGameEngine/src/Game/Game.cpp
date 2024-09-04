2342f2fc75e8bf2d5948d7254a4221969e1ccdcc
Lucas-HenriqueGoisSantos <lucas.henrique.gois.santos@gmail.com>
2024-09-04 09:35:51 -0300

    Adding a diferrent level and testing changing the level

--- Jade2DGameEngine/src/Game/Game.cpp
+++ Jade2DGameEngine/src/Game/Game.cpp
@@ -174,7 +174,7 @@ void Game::Setup() {
 
     lua.open_libraries( sol::lib::base, sol::lib::math, sol::lib::os );
 
-    loader.LoadLevel( lua, registry, assetStore, renderer, 1 );
+    loader.LoadLevel( lua, registry, assetStore, renderer, 2 );
 }
 
 void Game::Update() {
